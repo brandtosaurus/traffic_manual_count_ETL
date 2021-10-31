9a300e7c8fc1ef9a08cef69768af5ff3ec70cb2c
brandtosaurus <michael.craig.brandt@gmail.com>
2021-10-31 17:23:23 +0200

    dialogue box geting there

--- dialogbox.py
+++ dialogbox.py
@@ -3,7 +3,7 @@ from PyQt5.QtGui import QPixmap, QMouseEvent
 from PyQt5 import QtWidgets, uic
 import sys
 
-TYPES = ["TCS Trust", "Basic Format"]
+TYPES = ["", "TCS Trust", "Basic Format"]
 
 
 class Ui(QtWidgets.QDialog):
@@ -15,23 +15,52 @@ class Ui(QtWidgets.QDialog):
         self.typeComboBox.addItems(TYPES)
         self.typeComboBox.activated[str].connect(self.activated)
 
+        self.tchTrustImageExample.setOpenExternalLinks(True)
         self.tchTrustImageExample.setPixmap(QPixmap(r"assets\tcstrust_example.png"))
-        self.tchTrustImageExample.mousePressEvent.connect(self.activated)
+        self.tchTrustImageExample.linkActivated.connect(self.activated)
 
+        self.simpleImportExample.setOpenExternalLinks(True)
         self.simpleImportExample.setPixmap(QPixmap(r"assets\simpleImport_example.png"))
-        self.simpleImportExample.mousePressEvent.connect(self.activated)
+        self.simpleImportExample.linkActivated.connect(self.activated)
 
         self.chooseDirectory.clicked.connect(self._open_file_dialog)
 
+        self.runButton.clicked.connect(self.run)
+
+        self.buttonBox.rejected.connect(self.exit)
+
+        self.progressBar.setValue(0)
+
     def activated(self, text):
-        if self == self.tchTrustImageExample:
-            self.type = "TCS Trust"
-        elif self == self.simpleImportExample:
-            self.type = "Basic Format"
+        if text == "TCS Trust":
+            self.tchTrustImageExample.setStyleSheet(
+                "background-color: cyan; border: 3px solid red;"
+            )
+            self.simpleImportExample.setStyleSheet("background-color: rgba(0,0,0,0%)")
+        elif text == "Basic Format":
+            self.simpleImportExample.setStyleSheet(
+                "background-color: cyan; border: 3px solid red;"
+            )
+            self.tchTrustImageExample.setStyleSheet("background-color: rgba(0,0,0,0%)")
 
     def _open_file_dialog(self):
-        directory = str(QtWidgets.QFileDialog.getExistingDirectory())
-        self.textBar.setText("{}".format(directory))
+        path = str(QtWidgets.QFileDialog.getExistingDirectory())
+        self.textBar.setText("{}".format(path))
+
+    def run(self):
+        type = str(self.typeComboBox.currentText())
+        path = self.textBar.text()
+        if (type == "") or (type == None):
+            self.error_dialogue = QtWidgets.QErrorMessage()
+            self.error_dialogue.showMessage("Please select a type")
+        elif (path == "") or (path == None):
+            self.error_dialogue = QtWidgets.QErrorMessage()
+            self.error_dialogue.showMessage("Please select a folder")
+        else:
+            print("RUN")
+
+    def exit(self):
+        sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
